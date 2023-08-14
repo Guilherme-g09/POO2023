86103fd8a90e3db69f0f020e7fa6c31816fb0648
Guilherme Girelli <g.girelli@edu.pucrs.br>
2023-08-14 13:08:06 +0000

    Veiculos

---
+++ aula2/Veiculo.java
@@ -0,0 +1,71 @@
+public class Veiculo {
+    private final double consumoPorLitro = 10;
+    private Placa placa;
+    private double combustivel;
+    private double tamTanque;
+
+    public Veiculo(Placa placa, double tamTanque) {
+        this.placa = placa;
+        this.setTamTanque(tamTanque);
+        combustivel = 0;
+    }
+
+    public void setPais(String pais) {
+        placa.setPais(pais);
+    }
+
+    public Placa getPlaca() {
+        return placa;
+    }
+
+    public double getTanque() {
+        return tamTanque;
+    }
+
+    public void setTamTanque(double tamTanque) {
+        this.tamTanque = tamTanque;
+    }
+
+    public double getCombustivelNoTanque() {
+        return combustivel;
+    }
+
+    public double abastece(double litros) {
+        if (litros > 0.0) {
+            double tamTanqueDisponivel = tamTanque - combustivel;
+
+            if (tamTanqueDisponivel >= litros) {
+                combustivel += litros;
+            } else {
+                combustivel += tamTanqueDisponivel;
+            }
+
+        }
+        return combustivel;
+    }
+
+    // Simula o deslocamento do carro por uma determinada distancia em Km:
+    // - Diminui a quantidade de combustivel gasto do tanque
+    // - Retorna a distancia efetivamente percorrida (com o combustivel disponivel)
+    public double dirige(double distancia) {
+        // Para distancias impossíveis retorna 0
+        if (distancia <= 0) {
+            return 0.0;
+        }
+        // Calcula o consumo para a distancia
+        double consumo = distancia / consumoPorLitro;
+        // Diminui o combustivel gasto e retorna a distancia percorrida
+        if (combustivel >= consumo) {
+            combustivel -= consumo;
+            return distancia;
+        } else {
+            double distPossivel = combustivel * consumoPorLitro;
+            combustivel = 0.0;
+            return distPossivel;
+        }
+    }
+
+    public String toString() {
+        return "Placa: " + getPlaca() + ", combustivel no tanque: " + getCombustivelNoTanque();
+    }
+}
